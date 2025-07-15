9bcfad6b01924f08b04c1d8fe1e7b8d106f2a497
Ebed-meleck <ebedmeleckmakoso@gmail.com>
2025-07-15 11:38:48 +0100

    fix service

--- src/service/odk.service.ts
+++ src/service/odk.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getFromTest = async () => {
-  const url = `http://localhost:8092/api/submissions`;
+  const url = `https://odk.ebedmeleck.com/api/submissions`;
 
   const response = await axios.get(url);
   return response.data;
